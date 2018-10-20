a6f80ac7c5eae06579946c6f3b4628d0e8e5eef3
NextOne94 <44285038+NextOne94@users.noreply.github.com>
2018-10-20 16:29:59 +0700

    ยังไม่เสร็จ2

--- test111.py
+++ test111.py
@@ -4,7 +4,8 @@ arr.resize(16,22)
 arr1 = [(4,2),(4,14),(8,2),(12,10)]
 num1 = []
 num2 = []
-dall = []
+dall = np.arange(0)
+dall.resize(5,5)
 dAll = np.arange(0)
 dAll.resize(5,5)
 newarr = [ (0,0) ]
@@ -20,23 +21,35 @@ for i in range(len(newarr)) :
 print(num1) 
 print(num2)
 
-for i in range(len(num1)-1):
-        xi = np.power((num1[0]-num1[i+1]),2)
-        yi = np.power((num2[0]-num2[i+1]),2)
+for i in range(len(num1)):
+        xi = np.power((num1[0]-num1[i]),2)
+        yi = np.power((num2[0]-num2[i]),2)
         d = np.sqrt(xi+(yi))
-        dall.append(d)
-minn = dall[0]
+        dall[0][i] = d
+print(dall)
+minn = dall[0][0]
 position = 0
-for i in range(len(dall)):
-       if (minn > dall[i]):
-        minn = dall[i]
-        position = i
-        
+for i in range(len(num1)):
+        if minn == 0 :
+                minn =  dall[0][i]
+                position = i
+       
+        if minn > dall[0][0] and dall[0][0] > 0 :
+                minn = dall[i]
+                position = i
 
 print(dall)
 print(position)
 
+for i in range(len(num1)):
+        xi = np.power((num1[position]-num1[i]),2)
+        yi = np.power((num2[position]-num2[i]),2)
+        d = np.sqrt(xi+(yi))
+        dall[1][i] = d
+        
 
+
+print(dall)
 
 for i in range(len(num1)):
         for j in range(len(num1)):
